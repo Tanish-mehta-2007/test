cf2aef2fc18b9ab0e74d1194fc0cd1f39a10cfed
Tanish Mehta <mehtatanish11@gmail.com>
2025-05-19 14:42:34 +0530

    add logger

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 import express from "express"
 import 'dotenv/config'
-
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
+
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 
 app.use(express.json())
 let teadata = []
 let nextid = 1
 
 app.post("/teas",(req,res)=>{
+    logger.warn("A post request has been made to add a tea")
     const {name,price} = req.body
     const newTea = {id: nextid++, name, price}
     teadata.push(newTea) 
